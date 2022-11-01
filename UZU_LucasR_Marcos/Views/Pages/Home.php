5e7783b94fbf02cc5e45da756213ecf347393b15
Lucas Teixeira Ronchi <lucas0headshot@gmail.com>
2022-10-31 22:49:19 -0300

    Desenvolvimento Front-End 3/3
    Criação da Home, CSS próprio criado e definido

--- UZU_LucasR_Marcos/Views/Pages/Home.php
+++ UZU_LucasR_Marcos/Views/Pages/Home.php
@@ -6,10 +6,39 @@
     <link rel="preconnect" href="https://fonts.googleapis.com"> <!--Fonte Open Sans -->
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
-    <link href="<?php echo INCLUDE_PATH_STATIC ?>Styles/Style.css" rel="stylesheet">
+    <link href="<?php echo INCLUDE_PATH_STATIC ?>Styles/Feed.css" rel="stylesheet">
 </head>
 <body>
-    <div class = "sidebar"></div> <!-- Barra no canto esquerdo-->
+    <section class="main-feed"> 
+        <div class="sidebar"> <!-- Barra à esquerda -->
+            <div class="logo-sidebar"> <!-- Logo sidebar-->
+                <img src="<?php echo INCLUDE_PATH_STATIC?>Images/Logo - Preto.png"> 
+            </div>
+            <br/>
+            <div class="menu-sidebar">
+                <h4>Menu</h4>
+                <br/>
+                <a href="#">Feed</a>
+                <a href="#">Perfil</a>
+                <a href="#">Amigos</a>
+            </div>
+        </div> <!-- Sidebar -->
+
+        <div class="feed">
+            <div class="feed-single-post">
+                <div class="feed-single-post-author">
+                    <div class="img-single-post-author">
+                        <!-- Colocar imagem :D-->
+                    </div>
+                    <h3>Lucas</h3>
+                    <span>22:36 31/10/2022</span>
+                </div>
+                <div class="feed-single-post-content">
+                    <p>Teste, teste e mais testes...</p>
+                </div>
+            </div>
+        </div> <!-- Feed -->
+    </section>
     
 </body>
 </html>
