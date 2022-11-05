c1c2c56690627a3b9336d05f54d2da9cf8514864
Lucas Teixeira Ronchi <lucas0headshot@gmail.com>
2022-11-05 11:40:17 -0300

    .sql Banco de Dados
    Atualizado o Charset e agora funciona normalmente em outras versões do SQL

--- Backup_UZU.sql
+++ Backup_UZU.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 03-Nov-2022 às 10:23
+-- Tempo de geração: 05-Nov-2022 às 14:39
 -- Versão do servidor: 8.0.27
 -- versão do PHP: 7.4.26
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `uzu`
 --
+CREATE DATABASE IF NOT EXISTS `uzu` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `uzu`;
 
 -- --------------------------------------------------------
 
@@ -34,13 +36,16 @@ CREATE TABLE IF NOT EXISTS `amizades` (
   `Recebeu` int NOT NULL,
   `Status` int NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Extraindo dados da tabela `amizades`
 --
 
 INSERT INTO `amizades` (`ID`, `Enviou`, `Recebeu`, `Status`) VALUES
+(13, 8, 11, 1),
+(12, 8, 10, 1),
+(11, 8, 7, 1),
 (10, 7, 10, 1);
 
 -- --------------------------------------------------------
@@ -53,19 +58,20 @@ DROP TABLE IF EXISTS `posts`;
 CREATE TABLE IF NOT EXISTS `posts` (
   `ID` int NOT NULL AUTO_INCREMENT,
   `Usuario_ID` int NOT NULL,
-  `Post` text NOT NULL,
+  `Post` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `Data` datetime NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Extraindo dados da tabela `posts`
 --
 
 INSERT INTO `posts` (`ID`, `Usuario_ID`, `Post`, `Data`) VALUES
-(10, 8, '<p>Teste</p>', '2022-11-02 18:58:32'),
-(11, 8, '<p>Teste </p><img scr=\"https://cdn.eso.org/images/screen/millour-01-cc.jpg\" />', '2022-11-02 18:58:57'),
-(12, 8, '<p>Teste </p><img scr=\"https://cdn.eso.org/images/screen/millour-01-cc.jpg\" />', '2022-11-02 19:07:19');
+(30, 7, '<p>Hmmmmmmmmmmmmmm</p>', '2022-11-04 19:39:35'),
+(29, 11, '<p>Hm</p>', '2022-11-04 19:39:28'),
+(28, 10, '<p>Aoba</p>', '2022-11-04 19:39:15'),
+(27, 8, '<p>A</p>', '2022-11-04 19:38:59');
 
 -- --------------------------------------------------------
 
@@ -76,22 +82,24 @@ INSERT INTO `posts` (`ID`, `Usuario_ID`, `Post`, `Data`) VALUES
 DROP TABLE IF EXISTS `usuarios`;
 CREATE TABLE IF NOT EXISTS `usuarios` (
   `ID` int NOT NULL AUTO_INCREMENT,
-  `Nome` varchar(255) NOT NULL,
-  `Email` varchar(255) NOT NULL,
-  `Senha` text NOT NULL,
+  `Nome` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `Email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `Senha` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `Ultimo_Post` datetime NOT NULL,
+  `Img` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Extraindo dados da tabela `usuarios`
 --
 
-INSERT INTO `usuarios` (`ID`, `Nome`, `Email`, `Senha`, `Ultimo_Post`) VALUES
-(9, 'Guilherme', 'teste1@gmail.com', '$2a$08$MjAzOTkwOTg3NjYzNjFlYecBdxaoqM9fbrboCViW7uhQehEclqwSO', '0000-00-00 00:00:00'),
-(8, 'Teste', 'teste@gmail.com', '$2a$08$MTI5MTg4NzUyODYzNjA2ZOOI5oCutt87buHODCR7toiOYNLVuTHSS', '2022-11-02 19:07:19'),
-(7, 'Lucas', 'lucas0headshot@gmail.com', '$2a$08$MTI4ODA5Mjc2NjYzNjA2ZOsq0MnxzFZXFht2luKnJQoOYti4xo3Uy', '0000-00-00 00:00:00'),
-(10, 'Marcos', 'teste3@gmail.com', '$2a$08$NDE5NzUwODI2NjM2MWVlYOBcKIhrOPYN36Buyz8HexAd2s4XcX6S6', '0000-00-00 00:00:00');
+INSERT INTO `usuarios` (`ID`, `Nome`, `Email`, `Senha`, `Ultimo_Post`, `Img`) VALUES
+(9, 'Guilherme', 'teste1@gmail.com', '$2a$08$MjAzOTkwOTg3NjYzNjFlYecBdxaoqM9fbrboCViW7uhQehEclqwSO', '0000-00-00 00:00:00', ''),
+(8, 'Teste', 'teste@gmail.com', '$2a$08$MTI5MTg4NzUyODYzNjA2ZOOI5oCutt87buHODCR7toiOYNLVuTHSS', '2022-11-04 19:38:59', ''),
+(7, 'Lucas', 'lucas0headshot@gmail.com', '$2a$08$MTI4ODA5Mjc2NjYzNjA2ZOsq0MnxzFZXFht2luKnJQoOYti4xo3Uy', '2022-11-04 19:39:35', ''),
+(10, 'Marcos', 'teste3@gmail.com', '$2a$08$NDE5NzUwODI2NjM2MWVlYOBcKIhrOPYN36Buyz8HexAd2s4XcX6S6', '2022-11-04 19:39:15', ''),
+(11, 'Pietro', 'pietro0headshot@gmail.com', '$2a$08$MjUzMDI0MTEzNjM2NTdhMeC7tFnijS5.C9ISXvLZu8Y2dzb6o6rGm', '2022-11-04 19:39:28', '');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
